e99c2b4bbfb56d850c39698e82422aa88a7e4ed5
Cristian Vera <veracristianadrian@gmail.com>
2024-11-09 19:29:44 +0200

    ejemplo 4 modificado

--- url.js
+++ url.js
@@ -1,1 +1,2 @@
-var URL_OGC = '/cgi-bin/qgis_mapserv.fcgi?map=/usr/local/share/qgis/laboratorio.qgz'
+// var URL_OGC = '/cgi-bin/qgis_mapserv.fcgi?map=/usr/local/share/qgis/laboratorio.qgz'
+var URL_OGC = 'http://localhost/cgi-bin/qgis_mapserv.fcgi?map=/usr/local/share/qgis/trabajo-integrador.qgz'
